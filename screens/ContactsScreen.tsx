636cb9b20e51aaad44a5f28e852b361672505273
Pratham Bhargava <pratham.bhargava2018@vitbhopal.ac.in>
2021-01-18 18:37:44 +0530

    display users in userlist

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,26 +1,49 @@
-import * as React from 'react';
-import { StyleSheet} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
 
 import ContactListItem from "../components/ContactListItem"
-import users from  "../Data/Users"
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { FlatList } from 'react-native-gesture-handler';
 import NewMessageButton from '../components/NewMessageButton';
+import { graphqlOperation, API } from 'aws-amplify';
+
+import { listUsers } from "../src/graphql/queries"
+
 
 export default function ContactsScreen() {
+
+  const [users,setUsers] = useState([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const usersData = await API.graphql(
+          graphqlOperation(listUsers)
+        )
+        setUsers(usersData.data.listUsers.items);
+        console.log(usersData)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchUsers()
+  }, [])
+
+
+
   return (
     <View style={styles.container}>
       <FlatList
-      style = {{width:'100%'}}
-      
-      data = {users}
-      renderItem = {({item})=><ContactListItem user = {item}/>}
-      keyExtractor = {(item)=>item.id}
+        style={{ width: '100%' }}
+
+        data={users}
+        renderItem={({ item }) => <ContactListItem user={item} />}
+        keyExtractor={(item) => item.id}
       />
-      <NewMessageButton/>
-     </View>
+      <NewMessageButton />
+    </View>
   );
 }
 
